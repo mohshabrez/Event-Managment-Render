59202553ad8c04ecd166bc94f52307173a6095c7
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2024-01-20 18:16:05 +0530

    Update index

--- server/index.js
+++ server/index.js
@@ -21,9 +21,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", async (req, res) => {
-    res.send("Hello Event");
+app.get("/", (req, res) => {
+    res.send("Hello User");
   });
+
+  
+  app.use("/", (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong." });
+  });
+  
+  app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+  });
+  
 
 app.use("/api/v1", eventRouter);
 app.use("/api/v1", volunteerRouter);
