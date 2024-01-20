499fb49f8d34da0155d45d41c90de41999d6e2e1
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2024-01-20 18:29:18 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -23,21 +23,23 @@ app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello User");
-  });
-
-  
-  app.use("/", (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Something went wrong." });
-  });
-  
-  app.use((req, res) => {
-    res.status(404).json({ message: "Route not found" });
-  });
-  
+});
 
 app.use("/api/v1", eventRouter);
 app.use("/api/v1", volunteerRouter);
+
+  
+app.use("/", (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong." });
+});
+  
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+  
+
+
 
 
 // /* MONGOOSE SETUP */
